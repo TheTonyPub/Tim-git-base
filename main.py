6d799b647af6f391a7a77656144b85232016ff6c
Лена <plutalovaelenaa@gmail.com>
2026-09-07 20:08:17 +0300

    Оптимизация: замена рекурсии на итеративный цикл для ускорения и избежания переполнения стека

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
+    if n < 0:
+        return "Ошибка"
+
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 num = int(input("Введите число: "))
 print(f"{num}! = {factorial(num)}")
