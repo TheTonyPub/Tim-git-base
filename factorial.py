73d77f57e5d44648edae4ed11cf6f61f4c393d00
Uiqora <someqorats@gmail.com>
2026-09-08 21:12:27 +0300

    оптимизировал факториал через цикл

--- factorial.py
+++ factorial.py
@@ -1,7 +1,12 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
+def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError("Факториал отрицательного числа не определен")
 
-n = 5
-print("Факториал числа", n, "равен:", factorial(n))
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+if __name__ == "__main__":
+    num = 5
+    print(f"Факториал {num} = {factorial(num)}")
