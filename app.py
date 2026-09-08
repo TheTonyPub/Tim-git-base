8802ca4c3cda2f0ed96c7f58a4acf7e103474947
I3eUper <IKaKTus228@gmail.com>
2026-09-08 16:43:56 +0300

    Optimize factorial algorithm

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ def factorial(n):
     if n < 0:
         raise ValueError("n must be non-negative")
 
-    if n == 0:
-        return 1
+    result = 1
 
-    return n * factorial(n - 1)
+    for i in range(2, n + 1):
+        result *= i
+
+    return result
 
 
 if __name__ == "__main__":
