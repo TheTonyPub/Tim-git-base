8b68ac56ead3b439891088e69d80d07db8f8e3f6
JFTVlados89 <maxutca9@gmail.com>
2026-09-08 23:04:51 +0300

    Second version

--- Task.py
+++ Task.py
@@ -1,12 +1,14 @@
+from functools import *
+@lru_cache(None) #Применяем lru_cache чтобы запоминать результат работы функции, а не рассчитывать их заново 
+ 
 def fibonachi(n):
-    if n < 0:
+    if n<0:
         return None
-    if n == 1:
+    if n==1:
         return 0
-    if n == 2:
+    if n==2:
         return 1
-    return fibonachi(n - 2) + fibonachi(n - 1)   # рекурсия без запоминания
-
-a = [12, 13, 85, 34, 47] #Пользователь вписывает сам сюда числа, а не в косоле вбивает 
-b = [a[i] for i in range(len(a)) for y in range(1, 100) if fibonachi(y) == a[i]] #гинератор списка
+    return fibonachi(n-2)+fibonachi(n-1)
+a = list(map(int, input("Введите числа через пробел: ").split())) #Пользователь вводит числа через пробел
+b=[a[i] for i in range(len(a)) for y in range(1,100) if fibonachi(y)==a[i] ] 
 print(*b)
